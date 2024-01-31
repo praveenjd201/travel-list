1df8be054a4d885bfa5903b15af2f2669b4cbb2c
praveenjd201 <praveenjd201@gmail.com>
2024-01-31 18:50:16 +0530

    removed the defualt values

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,10 +4,7 @@ import Form from "./Form";
 import Packinglist from "./Packinglist";
 import Stats from "./Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
+const initialItems = [];
 
 function App() {
   const [items, setItems] = useState(initialItems); //lift state up to first common parent
